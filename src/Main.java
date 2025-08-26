d4039651720d536ae76aa279298fbe97af2ba397
SupremeSith <woof.portocarrero@gmail.com>
2025-08-26 11:27:19 -0300

    Implement Main class to instantiate Clients and test object creation
    - Created Main class with sample Clients objects
    - Printed client information for testing

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test my system!");
+        // objetos
+        // instancia da classe e infos do cleinte
+        Clients c001 = new Clients();
+        c001.firstName = "João Pedro";
+        c001.age = 18;
+
+        Clients c002 = new Clients();
+        c002.firstName = "Gabriel";
+        c002.age = 16;
+
+        Clients c003 = new Clients();
+        c003.firstName = "Maria";
+        c003.age = 18;
+
+
+        System.out.println(c002.firstName);
+
+
     }
 }
