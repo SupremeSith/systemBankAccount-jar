3a143724c74e08cdc40cbf46c6aa2fff0aaad576
SupremeSith <woof.portocarrero@gmail.com>
2025-08-26 11:26:41 -0300

    Add Clients class with basic attributes and account creation method - Implemented Clients class with fields: firstName and age - Added newAccount() method to simulate account creation

---
+++ src/Clients.java
@@ -0,0 +1,14 @@
+public class Clients {
+
+    // infos q vem da inha classe main
+    String firstName;
+    int age;
+
+    //metodos
+    void newAccount() {
+        System.out.println(firstName + "Account Created!");
+        System.out.println("The new balance is $10, congrats!!");
+    }
+
+
+}
